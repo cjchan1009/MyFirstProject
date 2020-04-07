2be8d604c52e0786b7e525d5dfd13fc3568abdc3
cjchan1009 <306703788@qq.com>
2020-04-07 23:01:29 +0800

    world bean

--- helloworld/src/World.java
+++ helloworld/src/World.java
@@ -1,4 +1,14 @@
 public class World {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
     public static void main(String[] args) {
         System.out.println("world");
     }
